ba3e732363cd1af612db5733366da920c08fb5ac
lck3nny <lck3nny@gmail.com>
2021-05-06 12:08:45 -0700

    moving SUrrey Stack project to repository

---
+++ database_init.sql
@@ -0,0 +1,8 @@
+create database surreystackdb;
+
+CREATE TABLE 'users'(
+	'user_id' int(11) NOT NULL AUTO_INCREMENT
+	'name' varchar(45) NOT NULL
+	'username' varchar(45) NOT NULL
+	PRIMARY KEY ('user_id')
+)	ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8
